9f544cdc5f082919016593a1d0bdf266ef540d2b
dslij@hotmail.com <Cuba2018>
2019-04-23 14:11:12 -0400

    first commit

---
+++ static/book/book.js
@@ -0,0 +1,43 @@
+const book = [{
+    content: 'Table of Contents',
+    Introduction: ['A Brief Introduction', '<h1>A Brief Introduction to the Course…</h1', `<p>In the state of Florida, anyone who does not possess a valid driver’s license from any other jurisdiction is required to take a 4-hour Traffic Law and Substance Abuse Education Course (TLSAE) in order to get a driver license for the first time.</p>
+    
+    <img src=".././images/c1/jpg" alt="pic" />
+    <p>
+    We all know that driving is a very complex task. It involves the integration and coordination of many skills and abilities.2 All drugs affect the way a person thinks and behaves; therefore, they can have a significant impact on a person’s ability to drive safely.3
+    </p>
+
+    <p>
+    This 4-hour course is designed to reduce the number of traffic fatalities related to
+    
+    impair driving, effects of impaired driving, penalties, consequences
+    
+    and ways to say NO to irresponsible driving behaviors. We will also
+    
+    be discussing safety for you and for you passengers riding with you, some basic State of Florida rules, regulations and traffic signs and signals. You are still responsible to study the Florida handbook to understand other State requirements before you get either your restricted license or the operator license if you are over 18 years of age. This course has been divided into three sections. Each section has been carefully planned for your learning experience.</p>`],
+    sectionI: [{
+        title: 'The use of the drugs and their effects on the body',
+        subtitle: 'What is a drug',
+        index: ['1.1 What is a drug?', '1.2 Alcoholism affects our society', '1.3 Types of drugs and their effects', '1.4 Polydrug use', '1.5 Long term effects of alcohol consuption', '1.6 Alcohol absorption', '1.7 Tolerance and dependence', '1.8 Steps toward addiction', '1.9 Psychological factors of use, abuse and additiction', 'End of the Section 1 – Review'],
+        content: ''
+      },
+      {
+        subtitle: 'Alcoholism affects our society',
+        page: '9'
+      },
+      {
+
+        subtitle: 'Types of drugs and their effects',
+        page: '11'
+      },
+      {
+        subtitle: 'Polydrug use',
+        page: 19
+      }
+    ]
+  },
+  {
+
+  }
+
+]
